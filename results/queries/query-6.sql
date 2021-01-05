4e331eaf15e132e1d2c9e5f926edf02d5b637a65
Cedric P <cedric.pages@le-campus-numerique.fr>
2021-01-05 14:44:31 +0100

    query-6

---
+++ results/queries/query-6.sql
@@ -0,0 +1,7 @@
+SELECT number, SUM(order_product.quantity * products.price) AS total
+FROM orders
+         INNER JOIN order_product
+                    ON orders.id = order_product.order_id
+         INNER JOIN products
+                    ON products.id = order_product.product_id
+GROUP BY orders.number
