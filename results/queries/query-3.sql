f6da1b20e76111bd36b7f994351dd72f965dd428
Cedric P <cedric.pages@le-campus-numerique.fr>
2021-01-05 11:57:01 +0100

    query-5

--- results/queries/query-3.sql
+++ results/queries/query-3.sql
@@ -1,5 +1,5 @@
 SELECT *
 FROM orders
-WHERE DATE(date) = DATE( NOW() ) -- datetime prend en compte la date du jour
+WHERE DATE(date) = DATE( NOW() ) -- date prend en compte la date du jour
 ORDER BY id DESC; -- on tri les numéros de la colonne 'number' par ordre décroissant(DESC) / croissant(ASC)
 
--- results/queries/query-5.sql
+++ results/queries/query-5.sql
@@ -1,6 +1,5 @@
---SELECT products.name
---FROM products
-         --INNER JOIN order_product
-                    --ON products.product_id = order_product.product_id
---WHERE order_product.order_id = '1';
-
+SELECT `name`,order_product.quantity AS quantity,`price`
+FROM products
+    INNER JOIN order_product
+        ON products.id = order_product.product_id
+WHERE order_product.order_id=1;
